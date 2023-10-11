ed40a6e7867be6909277483845b804a18fae6435
Angel Motta <angel.motta@utec.edu.pe>
2023-10-11 12:52:59 -0500

    feat: day18: debounce functions

---
+++ PromisesAndTime/day17-CacheWithTimeLimit-v2.js
@@ -0,0 +1,95 @@
+/**
+ * https://leetcode.com/problems/cache-with-time-limit/description/?envType=study-plan-v2&envId=30-days-of-javascript
+ * 
+ * Write a class that allows getting and setting key-value pairs, however a time until expiration is associated with each key.
+The class has three public methods:
+set(key, value, duration): accepts an integer key, an integer value, and a duration in milliseconds. Once the duration has elapsed, the key should be inaccessible. The method should return true if the same un-expired key already exists and false otherwise. Both the value and duration should be overwritten if the key already exists.
+get(key): if an un-expired key exists, it should return the associated value. Otherwise it should return -1.
+count(): returns the count of un-expired keys.
+
+Example 1:
+
+Input: 
+actions = ["TimeLimitedCache", "set", "get", "count", "get"]
+values = [[], [1, 42, 100], [1], [], [1]]
+timeDelays = [0, 0, 50, 50, 150]
+Output: [null, false, 42, 1, -1]
+Explanation:
+At t=0, the cache is constructed.
+At t=0, a key-value pair (1: 42) is added with a time limit of 100ms. The value doesn't exist so false is returned.
+At t=50, key=1 is requested and the value of 42 is returned.
+At t=50, count() is called and there is one active key in the cache.
+At t=100, key=1 expires.
+At t=150, get(1) is called but -1 is returned because the cache is empty.
+Example 2:
+
+Input: 
+actions = ["TimeLimitedCache", "set", "set", "get", "get", "get", "count"]
+values = [[], [1, 42, 50], [1, 50, 100], [1], [1], [1], []]
+timeDelays = [0, 0, 40, 50, 120, 200, 250]
+Output: [null, false, true, 50, 50, -1, 0]
+Explanation:
+At t=0, the cache is constructed.
+At t=0, a key-value pair (1: 42) is added with a time limit of 50ms. The value doesn't exist so false is returned.
+At t=40, a key-value pair (1: 50) is added with a time limit of 100ms. A non-expired value already existed so true is returned and the old value was overwritten.
+At t=50, get(1) is called which returned 50.
+At t=120, get(1) is called which returned 50.
+At t=140, key=1 expires.
+At t=200, get(1) is called but the cache is empty so -1 is returned.
+At t=250, count() returns 0 because the cache is empty.
+ */
+
+// Approach #2: Using callbacks
+// key: integer
+// Value: {value: int, timerId: int}
+var TimeLimitedCache = function () {
+    this.cache = new Map();
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+    // Special case: return true if the same un-expired key already exists
+    // Normal case: return false
+    // Prepare new payload
+    const timerId = setTimeout(() => this.cache.delete(key), duration); // define new expiration time
+    const valObj = { value, timerId };
+    if (this.cache.has(key)) {
+        // avoid an unwanted future delete from previous callback
+        const existingTimerId = this.cache.get(key).timerId;
+        clearTimeout(existingTimerId);
+        // Overwrite new value
+        this.cache.set(key, valObj);
+        return true;
+    }
+    // Insert new data to DS
+    this.cache.set(key, valObj);
+    return false;
+};
+
+/**
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function (key) {
+    if (this.cache.has(key)) {
+        return this.cache.get(key).value;
+    }
+    return -1;
+};
+
+/**
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function () {
+    return this.cache.size;
+};
+
+const timeLimitedCache = new TimeLimitedCache();
+console.log(timeLimitedCache.set(1, 42, 1000)); // false
+console.log(timeLimitedCache.get(1)); // 42
+console.log(timeLimitedCache.count()); // 1
