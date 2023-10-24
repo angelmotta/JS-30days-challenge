b9c5a6a26859b7826c3d06247a991a99f34e2bcd
Angel Motta <angel.motta@utec.edu.pe>
2023-10-23 21:31:57 -0500

    feat: day 26 - additional approach compactObject problem

--- Json/day26-CompactObject.js
+++ Json/day26-CompactObject.js
@@ -26,6 +26,7 @@ Explanation: obj[0], obj[1], obj[3][0], and obj[4][0] were falsy and removed.
  * @param {Object|Array} obj
  * @return {Object|Array}
  */
+// Approach #1: my original implementation
 var compactObject = function (obj) {
     let arrKeys = Object.keys(obj);
     let res = {};
@@ -35,7 +36,7 @@ var compactObject = function (obj) {
     for (const key of arrKeys) {
         if (Boolean(obj[key])) {
             if (Array.isArray(obj[key]) || typeof obj[key] === "object") {
-                resObj = compactObject(obj[key]);
+                resObj = compactObject(obj[key]); // recursive call
                 if (Array.isArray(res)) {
                     res.push(resObj);
                 } else {
@@ -54,6 +55,59 @@ var compactObject = function (obj) {
 };
 
 let obj = [null, 0, false, 1];
+// console.log(compactObject(obj));
+
+obj = { a: 1, b: [false, 2], c: null, d: "d" };
+// console.log(compactObject(obj));
+
+obj = [null, 0, 5, [0], [false, 16]];
+// console.log(compactObject(obj));
+
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+// Approach #2: using a base case and clearer DFS approach
+compactObject = function (obj) {
+    // Base case: return false or primitive data type
+    if (Boolean(obj) === false) {
+        // filter null Object and falsy values
+        return false;
+    }
+    if (typeof obj !== "object") {
+        // obj is a primitve data type (Number, string, etc...)
+        return obj;
+    }
+    // obj is an Array or Object
+    function dfs(obj) {
+        // Handle Array type
+        if (Array.isArray(obj)) {
+            const arrData = [];
+            for (const elem of obj) {
+                const response = compactObject(elem); // recursive call
+                if (response) {
+                    // Valid non-falsy value
+                    arrData.push(response);
+                }
+            }
+            return arrData;
+        }
+
+        // Handle Object type
+        const objData = {};
+        for (const key in obj) {
+            const response = compactObject(obj[key]);
+            if (response) {
+                objData[key] = response;
+            }
+        }
+        return objData;
+    }
+
+    return dfs(obj);
+};
+
+obj = [null, 0, false, 1];
 console.log(compactObject(obj));
 
 obj = { a: 1, b: [false, 2], c: null, d: "d" };
